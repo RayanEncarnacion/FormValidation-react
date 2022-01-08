51ab199244780f143de1084e4d25c39c133385b4
Rayan Encarnacion <rayanbalbuena@gmail.com>
2022-01-08 18:38:36 -0400

    Fixing validation error messages

--- src/registerSchema.js
+++ src/registerSchema.js
@@ -4,15 +4,9 @@ export default yup.object().shape({
   name: yup
     .string()
     .trim()
-    .matches(/^[a-z A-Z]+$/, "Name can not be empty or contain numbers.")
-    .required("Your name is required!"),
-  email: yup.string().trim().email().required("Email must be valid."),
-  password: yup
-    .string()
-    .trim()
-    .min(4)
-    .max(16)
-    .required("Passwords must be the same."),
+    .matches(/^[a-z A-Z]+$/, "Name can not be empty or contain numbers."),
+  email: yup.string().trim().email(),
+  password: yup.string().trim().min(4).max(16),
   repeatPassword: yup
     .string()
     .oneOf([yup.ref("password"), null], "Passwords must be the same."),
